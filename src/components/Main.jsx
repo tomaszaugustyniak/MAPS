1436ab36888e8d0b333dc333d2ed2320721ebd47
tomaszaugustyniak <tomasz.augustyniak2000@gmail.com>
2022-04-21 16:16:59 +0200

    first changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,33 @@
 import React, {useState, useEffect} from 'react'
+import { Typography,  CssBaseline,  Container, Input, TextField, Grid, Paper, makeStyles } from '@material-ui/core'
 
-import { Typography,  CssBaseline,  Container, Input, TextField, Grid, } from '@material-ui/core'
+
 const initialValues = {
     startingPoint :'',
     destinationPoint: '',
 }
+
+const useStyle = makeStyles(theme =>({
+    root: {
+        '& .MuiFormControl-root' : {
+        width: '80%',
+        margin:theme.spacing(1)
+        }
+    }
+}))
 
 
 
 export default function Main() {
 
     const [values, setValues] = useState(initialValues);
+    const classes =  useStyle();
 
   return (
     <>
         <main>
             <div>
+                
                 <Container maxWidth="sm" gutterBottom>
                 <Typography variant="h2" align="center" gutterBottom>
                          Hello, and Welcome to MAPS
@@ -24,21 +36,22 @@ export default function Main() {
                          </Typography>
                     </Typography>
                 </Container>
+                <Paper>
                 
-                <CssBaseline/>
-                <Container maxWidth='md' className='menu'>
-                    <form>
-                        
-                    <Grid container align="center">
-                        <Grid item xs >
-                            <TextField variant="outlined" label="Starting Point" value={values.startingPoint}/>
+                    <Container maxWidth='md' className='menu'>
+                        <form className='classes.root'>
+                            
+                        <Grid container align="center">
+                            <Grid item xs >
+                                <TextField variant="outlined" label="Starting Point" value={values.startingPoint}/>
+                            </Grid>
+                            <Grid item xs>
+                                <TextField variant="outlined" label="Destination Point" value={values.destinationPoint}/>
+                            </Grid>
                         </Grid>
-                        <Grid item xs>
-                            <TextField variant="outlined" label="Destination Point" value={values.destinationPoint}/>
-                        </Grid>
-                    </Grid>
-                    </form>
-                </Container>
+                        </form>
+                    </Container>
+                </Paper>
             </div>
         </main>
     </>
